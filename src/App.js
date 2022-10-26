298680aef6fe088f8bfe7ce0783af83eddfb7dcd
wonil <jce951008@naver.com>
2022-10-26 16:44:33 +0900

    wonil commit #2 footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import Header from "./components/header";
+import React from "react";
+import Header from "./Components/header.jsx";
+import Nav from "./Components/nav.jsx";
+import Footer from "./Components/footer.jsx";
+
 function App() {
     return (
         <div>
             <Header />
+            <Nav />
+
+            <Footer />
         </div>
     );
 }
