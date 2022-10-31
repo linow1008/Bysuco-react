88135c825a965d01ea1198e8eee5c9038cc0bf6e
linow1008 <jce951008@naver.com>
2022-10-31 09:35:04 +0900

    wonil commit #6

--- src/components/layout/nav.jsx
+++ src/components/layout/nav.jsx
@@ -1,20 +1,66 @@
 import React from "react";
+import styled from "styled-components";
 import tw from "twin.macro";
 import NavDropdown from "./navDropdown";
-import NavSubmenu from "./navSubmenu";
 
-const Nav = tw.div`
-    flex justify-between items-center w-[80%] mx-auto h-[3.125rem] text-[.875rem] text-white
+const MoNav = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 50px;
+    color: #fff;
+    background-color: #006344;
+    white-space: nowrap;
+    overflow-x: auto;
+    font-size: 14px;
+    ::-webkit-scrollbar {
+        width: 150px;
+    }
+    ::-webkit-scrollbar-track {
+    }
+    div {
+        display: inline-block;
+    }
 `;
 
 const nav = () => {
     return (
-        <div className="bg-myGreen w-full">
-            <Nav>
-                <NavSubmenu />
-                <NavDropdown />
-            </Nav>
-        </div>
+        <>
+            {/* 모바일 Nav*/}
+            <MoNav className="space-x-[20px] pt-3 px-5">
+                <div>
+                    <a href="#">HOME</a>
+                </div>
+                <div>
+                    <a href="#">SHOP</a>
+                </div>
+                <div>
+                    <a href="#">BRAND</a>
+                </div>
+                <div>
+                    <a href="#">타임세일</a>
+                </div>
+                <div>
+                    <a href="#">래플이벤트</a>
+                </div>
+                <div>
+                    <a href="#">NEW</a>
+                </div>
+                <div>
+                    <a href="#">BEST</a>
+                </div>
+                <div>
+                    <a href="#">EVENT</a>
+                </div>
+                <div>
+                    <a href="#">COMMUNITY</a>
+                </div>
+                <div>
+                    <a href="#">ByYou</a>
+                </div>
+            </MoNav>
+            {/* PC Nav */}
+        </>
     );
 };
 
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import Header from "../components/layout/header.jsx";
 import Nav from "../components/layout/nav.jsx";
 import Footer from "../components/layout/footer.jsx";
-import Timesale from "../components/timesale.jsx";
 
 const page = () => {
     return (
         <div>
             <Header />
             <Nav />
-            <Timesale />
             <Footer />
         </div>
     );
